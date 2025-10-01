9de5ea17c7168dd8d183f5bd94f7bcea271b3798
cuzimapanda2003 <marcblais03@gmail.com>
2025-10-01 14:18:44 -0400

    Create Laveuse.cs

---
+++ ConsoleApp1/tp01/Laveuse.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp01
+{
+    public class Laveuse
+    {
+        private TasDeLinge? tas;
+        public TasDeLinge? Tas
+        {
+            get { return tas; }
+            set
+            {
+                tas = value;
+            }
+        }
+        public TasDeLinge? DonneLinge()
+        {
+            TasDeLinge? linge = tas;
+            tas = null;
+            return linge;
+        }
+        public bool Rincage { get; set; } = false;
+        public int Chaleur { get; set; } = 0;
+
+
+        public void Affiche()
+        {
+            Console.WriteLine(this.GetType().Name + " :");
+            if (Tas == null)
+            {
+                Console.WriteLine("  null");
+            }
+            else
+            {
+                Tas.Affiche();
+            }
+
+            string eau = (Chaleur == 0) ? "eau froide" : "eau chaude";
+            Console.WriteLine($"  {eau}, rinçage = {Rincage}");
+        }
+
+        public void Laver()
+        {
+            string eau = (Chaleur == 0) ? "eau froide" : "eau chaude";
+            Console.WriteLine($"La laveuse lave avec {eau}.");
+
+            if (Tas != null)
+            {
+                Console.WriteLine("Tas de linge dans la laveuse :");
+                Tas.Affiche();
+            }
+            else
+            {
+                Console.WriteLine("Il n’y a rien à laver !");
+            }
+
+            if (Rincage)
+            {
+                Console.WriteLine("Rinçage effectué.");
+            }
+        }
+    }
+}
