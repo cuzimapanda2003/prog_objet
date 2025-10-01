83ec520970fd04b6978410fe14954e2d04c1ec21
cuzimapanda2003 <marcblais03@gmail.com>
2025-10-01 14:17:07 -0400

    Create Panier.cs

---
+++ ConsoleApp1/tp01/Panier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp01
+{
+    public class Panier
+    {
+        public TasDeLinge? Tas { get; private set; }
+        public void PrendLinge(TasDeLinge tas)
+        {
+            tas.Lieux = "Panier";
+            Tas = tas;
+        }
+        public TasDeLinge? DonneLinge()
+        {
+            TasDeLinge? linge = Tas;
+            Tas = null; 
+            return linge;
+        }
+
+        public void Affiche()
+        {
+            Console.WriteLine(this.GetType().Name + " :");
+            if (Tas == null)
+            {
+                Console.WriteLine("  null");
+            }
+            else
+            {
+                Tas.Affiche();
+            }
+        }
+
+     }
+}
