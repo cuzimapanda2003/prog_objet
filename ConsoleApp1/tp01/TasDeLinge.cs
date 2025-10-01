8202b5fa8d4606780c8a7414860d986978c71e46
cuzimapanda2003 <marcblais03@gmail.com>
2025-10-01 14:17:01 -0400

    Create TasDeLinge.cs

---
+++ ConsoleApp1/tp01/TasDeLinge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp01
+{
+    public class TasDeLinge
+    {
+        public string Lieux { get; set; }
+        public string Couleur { get; set; }
+
+
+        public TasDeLinge(string couleur)
+        {
+            Couleur = couleur;
+            Lieux = "lit";
+        }
+
+        public void Affiche()
+        {
+            Console.WriteLine($"{this.GetType().Name} : {Couleur} {Lieux}");
+        }
+
+
+    }
+}
