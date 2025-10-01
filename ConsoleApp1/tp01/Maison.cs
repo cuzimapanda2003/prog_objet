8ba17d24ef6e2012eda41a76d082a9025fbb7054
cuzimapanda2003 <marcblais03@gmail.com>
2025-10-01 14:18:43 -0400

    Create Maison.cs

---
+++ ConsoleApp1/tp01/Maison.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp01
+{
+    public class Maison
+    {
+        public TasDeLinge[] tdl { get; set; } = new TasDeLinge[2];
+        public CordeALinge? cl { get; set; } = null;
+        public Panier p { get; set; } = new Panier();
+        public Laveuse? la { get; set; } = null;
+        public Personne? pe { get; set; } = null;
+
+
+        public Maison()
+        {
+            pe = new Personne(this);
+            tdl[0] = new TasDeLinge("rouge");
+            tdl[1] = new TasDeLinge("blanc");
+        }
+
+
+        public void Affiche()
+        {
+            Console.WriteLine(this.GetType().Name + " :");
+
+            foreach (var tas in tdl)
+            {
+                tas.Affiche();
+            }
+
+            p.Affiche();
+
+            if (la != null)
+                la.Affiche();
+
+            if (cl != null)
+                cl.Affiche();
+        }
+    
+    }
+}
