9d03278b55b44a75d30f3940d16d53ef756ffdf6
cuzimapanda2003 <marcblais03@gmail.com>
2025-10-01 14:18:47 -0400

    Create CordeALinge.cs

---
+++ ConsoleApp1/tp01/CordeALinge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp01
+{
+    public class CordeALinge
+    {
+        public TasDeLinge[] LesTas { get; set; } = new TasDeLinge[3];
+
+
+        public void Affiche()
+        {
+            Console.WriteLine(this.GetType().Name + " :");
+            for (int i = 0; i < LesTas.Length; i++)
+            {
+                if (LesTas[i] == null)
+                {
+                    Console.WriteLine($"  Case {i} : null");
+                }
+                else
+                {
+                    Console.Write($"  Case {i} : ");
+                    LesTas[i]?.Affiche();
+                }
+
+            }
+        }
+    }
+}
