9fe8bfb86d1fbbd2ff23c099d71c06cb63a40efc
cuzimapanda2003 <marcblais03@gmail.com>
2025-10-01 14:19:15 -0400

    tp01

--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/cours1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/cours1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cours1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c78116ae2f509fab6e680b4be75a5ddf7d759a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f1301745a5f00a1de62e743620dbfcab04e3edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("cours1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cours1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
