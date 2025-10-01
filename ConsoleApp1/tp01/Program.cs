555f20a1133dffb628dc818e02ae04a411c10d34
cuzimapanda2003 <marcblais03@gmail.com>
2025-10-01 14:17:03 -0400

    Create Program.cs

---
+++ ConsoleApp1/tp01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace tp01
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Maison maison = new Maison();
+            Laveuse laveuse = new Laveuse();
+            maison.la = laveuse;
+            Console.WriteLine("Affichage initial de la maison :");
+            maison.Affiche();
+            Console.WriteLine();
+            if (maison.p != null)
+            {
+                maison.pe.FaireTonLavage();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Affichage final de la maison :");
+            maison.Affiche();
+            Console.ReadLine();
+
+        }
+    }
+}
